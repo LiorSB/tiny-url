a4b82a09942f4e5e913006ed23e9cfcd9102db67
OmriKellnerMoveo <omrik@moveo.co.il>
2022-09-17 17:00:00 +0300

    add flask

--- back_end.py
+++ back_end.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1> Hello World!</h1>'
+
+@app.route('/users/<name>')
+def user(name):
+    return '<h1>hello, {}!</h1>'.format(name)
