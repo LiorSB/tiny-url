55304c85581d5ae25314ae62a5d1d3a62bc3a0b1
OmriKellnerMoveo <omrik@moveo.co.il>
2022-10-05 15:54:40 +0100

    connect front into back

--- tiny-url/src/pages/main-panel/MainPanel.js
+++ tiny-url/src/pages/main-panel/MainPanel.js
@@ -8,7 +8,7 @@ export const MainPanel = () => {
         <Header/>
         <div className={"forms-wrapper"}>
             <Form title="Enter a long URL to make a TinyUrl" inputLabel={"Enter Original URL: "}
-                  buttonLabel={"Make TinyUrl! (post request)"} func={postCreateTinyUrl}/>
+                  buttonLabel={"Make Tiny URL! (post request)"} func={postCreateTinyUrl}/>
             <Form title="Enter a tiny URL to get the original url" inputLabel={"Enter Tiny URL: "}
                   buttonLabel={"Get Original URL! (post request)"} func={postRedirectBySendTinyUrl}/>
         </div>
