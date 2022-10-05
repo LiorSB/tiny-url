e5bbf475a83260776d10c9af8eefb64e69d9e533
OmriKellnerMoveo <omrik@moveo.co.il>
2022-10-05 15:10:48 +0100

    connect front into back

--- tiny-url/src/pages/main-panel/components/form/Form.js
+++ tiny-url/src/pages/main-panel/components/form/Form.js
@@ -1,18 +1,24 @@
 import './Form.css'
 import {useState} from "react";
-export const Form=()=>{
+import {getTinyUrl, postCreateTinyUrl} from "../../../../html-request";
+
+export const Form = () => {
     const [name, setName] = useState("");
+    const [tinyUrlObj, setTinyUrlObj] = useState({tinyUrl:'',srcUrl:"",creationDate:""});
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`The name you entered was: ${name}`);
+        postCreateTinyUrl(name, setTinyUrlObj).then()
     }
 
+    const postFunc = () => {
+        getTinyUrl().then()
+    }
     return <div className="form-container">
         <span className="form-title-style">Enter a long URL to make a TinyUrl</span>
         <form className="form-style" onSubmit={handleSubmit}>
             <div>
-                <label>Enter Original Url:  </label>
+                <label>Enter Original Url: </label>
                 <input
                     className="form-input-style"
                     type="text"
@@ -21,7 +27,15 @@ export const Form=()=>{
                 />
             </div>
 
-          <button type="submit" className="button-submit-form">Make TinyUrl!</button>
+            <button type="submit" className="button-submit-form">Make TinyUrl!(post request)</button>
         </form>
+        <button className="button-submit-form" onClick={() => postFunc()}>(get request)</button>
+        {tinyUrlObj.tinyUrl.length>0 && <div className="tiny-url-container">
+
+
+           <span>Tiny Url : {tinyUrlObj.tinyUrl}</span>
+           <span>Original Url : {tinyUrlObj.srcUrl}</span>
+           <span>Creation Time : {tinyUrlObj.creationDate}</span>
+        </div>}
     </div>
 }
